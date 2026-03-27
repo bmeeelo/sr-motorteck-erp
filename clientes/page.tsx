9b774310f3022fb5d56e04c2a0a33520287c56d9
bmeeelo <bruno_melu@hotmail.com>
2026-03-27 14:56:59 -0300

    Delete clientes/page.tsx

--- clientes/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ClientesPage() {
-  return (
-    <main style={{ padding: "20px", color: "white" }}>
-      <h1>Clientes</h1>
-      <p>Página funcionando.</p>
-    </main>
-  );
-}
